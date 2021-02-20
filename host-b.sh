bffd28f08f107082ea0ef3f5d99e6d1a51b270ee
calorechiara <74462731+calorechiara@users.noreply.github.com>
2021-02-20 08:57:47 +0100

    Update host-b.sh

--- host-b.sh
+++ host-b.sh
@@ -12,6 +12,7 @@ sudo docker run --name nginx -p 80:80 -d dustnic82/nginx-test
 #Network interface config
 sudo ip addr add 192.168.2.2/23 dev enp0s8
 sudo ip link set dev enp0s8 up
+sudo tc qdisc add dev enp0s8 root tbf rate 1mbit burst 32kbit latency 400ms
 #Defaul gateway set up
 sudo ip route add 192.168.0.0/22 via 192.168.2.1
 
