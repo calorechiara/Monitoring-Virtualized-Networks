bcb85f0cd7ebd416b54a08650d007b27a7a85860
calorechiara <74462731+calorechiara@users.noreply.github.com>
2021-02-20 08:57:08 +0100

    Update router-1.sh

--- router-1.sh
+++ router-1.sh
@@ -8,5 +8,7 @@ sudo ip link add link enp0s8 name enp0s8.2 type vlan id 2
 sudo ip link add link enp0s8 name enp0s8.3 type vlan id 3
 sudo ip addr add 192.168.0.1/23 dev enp0s8.2
 sudo ip addr add 192.168.2.1/23 dev enp0s8.3
+sudo tc qdisc add dev enp0s8.2 root tbf rate 1mbit burst 32kbit latency 400ms
+sudo tc qdisc add dev enp0s8.3 root tbf rate 1mbit burst 32kbit latency 400ms
 sudo ip link set dev enp0s8 up
 
